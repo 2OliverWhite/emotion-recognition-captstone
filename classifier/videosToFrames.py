5d9923f8f71a08299d5a75298a1f5a5e8bbc7c58
Oliver White <oliver-white@live.com>
2023-03-20 21:35:36 +1000

    [FIX] finish script to extract videos

--- classifier/videosToFrames.py
+++ classifier/videosToFrames.py
@@ -1,12 +1,12 @@
-# import torch
-# from torchvision import datasets
-# import torch.nn as nn
-# import torch.optim as optim
+import torch
+from torchvision import datasets
+import torch.nn as nn
+import torch.optim as optim
 import os 
 import numpy as np
 import argparse
 from PIL import Image
-# import cv2
+import cv2
 
 
 
@@ -23,9 +23,9 @@ parser.add_argument("--train_root", help="training data directory",
 parser.add_argument("--valid_root", help="validation data directory",
                     type=str, default="../images/train_three/")
 parser.add_argument("--video_dir", help="src of videos to extract frames from",
-                    type=str, default="./data/videos")
+                    type=str, default="./data/videos/training")
 parser.add_argument("--save_dir", help="place to put extracted frames",
-                    type=str, default="./data/images")
+                    type=str, default="./data/training/images")
 args = parser.parse_args()
 
 
@@ -34,25 +34,25 @@ def extract(model, video_dir, save_dir):
 
     for emotion_class in os.listdir(video_dir):
         frame_path = f"{save_dir}/{emotion_class}"
+        print(frame_path)
         if not os.path.exists(frame_path):
             os.makedirs(frame_path)
         for vid in os.listdir(video_dir + '/' +  emotion_class):
-            print(f"Vid is {vid}")
             # cap = cv2.VideoCapture(vid)
             vname = vid.split('/')[-1] # Split filename from path (./hello/video.mp4 => video.mp4)
             vname = vname.split('.')[0] # Cutoff file extension (video.mp4 => video)
             frameCount = 0
             while True:
-                print(vname)
-                break
-                # ret, frame = cap.read()
-                # if not ret:
-                #     break
-                #if frameCount % 5 == 0:
-                    # img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    # im_pil = Image.fromarray(img)
+                newFilename = f"{frame_path}/{vname}_{frameCount}.jpg"
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                if frameCount % 5 == 0:
+                    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    im_pil = Image.fromarray(img)
+                    im_pil.save(newFilename)
                     
-                #frameCount += 1
-                # cap.release()
+                frameCount += 1
+            cap.release()
             
 extract(0, args.video_dir, args.save_dir)
